34f01cc1a067d069305579114f650463efc5209e
xvi <jrip03@gmail.com>
2020-12-30 21:05:42 -0500

    payload 602 encode

--- assembly/index.ts
+++ assembly/index.ts
@@ -4,53 +4,40 @@ export function add(a: i32, b: i32): i32 {
   return a + b;
 }
 
-export function UuidGen602(): String {
-      var o = "";
-      if (r)
-        try {
-          for (
-            var i = (new Date().getTime() * Math.random() + "")
-                .replace(".", ".".charCodeAt())
-                .split("")
-                .slice(-16),
-              a = 0;
-            a < i.length;
-            a++
-          )
-            i[a] =
-              parseInt(10 * Math.random()) * +i[a] ||
-              parseInt(Math.random() * Eu.len);
-          o = cn(i, 0, Eu.cipher);
-        } catch (t) {}
-      var c = (n && e) || 0,
-        u = n || [];
-      t = t || {};
-      var f = void 0 !== t.clockseq ? t.clockseq : Nu,
-        s = void 0 !== t.msecs ? t.msecs : m(),
-        d = void 0 !== t.nsecs ? t.nsecs : Zu + 1,
-        l = s - Wu + (d - Zu) / 1e4;
-	  // Why does this work in JS and not TS
-      if (
-        (l < 0 && void 0 === t.clockseq && (f = (f + 1) & 16383),
-        (l < 0 || s > Wu) && void 0 === t.nsecs && (d = 0),
-        d >= 1e4)
-      )
-        throw new Error("uuid.v1(): Can't create more than 10M uuids/sec");
-      (Wu = s), (Zu = d), (Nu = f), (s += 122192928e5);
-      var v = (1e4 * (268435455 & s) + d) % 4294967296;
-      (u[c++] = (v >>> 24) & 255),
-        (u[c++] = (v >>> 16) & 255),
-        (u[c++] = (v >>> 8) & 255),
-        (u[c++] = 255 & v);
-      var X = ((s / 4294967296) * 1e4) & 268435455;
-      (u[c++] = (X >>> 8) & 255),
-        (u[c++] = 255 & X),
-        (u[c++] = ((X >>> 24) & 15) | 16),
-        (u[c++] = (X >>> 16) & 255),
-        (u[c++] = (f >>> 8) | 128),
-        (u[c++] = 255 & f);
-      for (var p = t.node || Ou, h = 0; h < 6; h++) u[c + h] = p[h];
-      var P = n || cn(u);
-      return o === P ? o : (S("PX505"), P);
+export function payloadGen602(t, site, appid, uuid, seq, cs, sid, vid, pxhd): String {
+    console.log(t);
+    seq = seq || 0;
+    var n = t[0],
+        e = n && n.d;
+    e && (e.PX96 = site)
+    debugger;
+    var i = JSON.stringify.apply(null, Array.prototype.slice.call([t])),
+        a = ru(rn(i, 50)),
+        c = ["payload=" + a, "appId=" + appid, "tag=" + "v6.0.2", "uuid=" + uuid, "ft=" + "179", "seq=" + seq, "en=NTA"];
+    cs && c.push("cs=" + cs);
+    var s;
+    var jasper = z(i, [uuid, "v6.0.2", "179"].join(":"));
+    for (var n = "", e = "", r = 0; r < jasper.length; r++) {
+        var o = jasper.charCodeAt(r);
+        o >= 48 && o <= 57 ? n += jasper[r] : e += o % 10
+    }
+    var expedia = n + e
+    try {
+        for (var r = expedia, o = "", flex = 0; flex < r.length; flex += 2)
+            o += r[flex];
+        s = "" + o;
+    } catch (t) {}
+    c.push("pc=" + s.replace(/0{3,}[1-9]/, '')); //pc
+    sid != undefined && c.push("sid=" + sid); //sid
+    vid != undefined  && c.push("vid=" + vid); //vid
+    pxhd  != undefined && c.push("pxhd=" + pxhd); //pxhd cookie
+    c.push('rsc=' + (seq <= 1 ? seq+1 : seq));
+
+    return c
 }
+
+export function payloadGen621(): String {
+
+}
+
 
