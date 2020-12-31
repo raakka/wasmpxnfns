908ba341556078b8f0c537dbc87730fa2360c681
xvi <jrip03@gmail.com>
2020-12-30 22:12:03 -0500

    removing wasm files | updating TS types

--- assembly/index.ts
+++ assembly/index.ts
@@ -4,21 +4,20 @@ export function add(a: i32, b: i32): i32 {
   return a + b;
 }
 
-export function payloadGen602(t, site, appid, uuid, seq, cs, sid, vid, pxhd): String {
+export function payloadGen602(t: Array<string>, site: string, appid: string, uuid: string, seq:	number, cs: string, sid: string, vid: string, pxhd: string): string {
     console.log(t);
     seq = seq || 0;
     var n = t[0],
         e = n && n.d;
     e && (e.PX96 = site)
-    debugger;
     var i = JSON.stringify.apply(null, Array.prototype.slice.call([t])),
         a = ru(rn(i, 50)),
         c = ["payload=" + a, "appId=" + appid, "tag=" + "v6.0.2", "uuid=" + uuid, "ft=" + "179", "seq=" + seq, "en=NTA"];
     cs && c.push("cs=" + cs);
-    var s;
-    var jasper = z(i, [uuid, "v6.0.2", "179"].join(":"));
+    var s = "";
+    var pl = z(i, [uuid, "v6.0.2", "179"].join(":"));
     for (var n = "", e = "", r = 0; r < jasper.length; r++) {
-        var o = jasper.charCodeAt(r);
+        var o = pl.charCodeAt(r);
         o >= 48 && o <= 57 ? n += jasper[r] : e += o % 10
     }
     var expedia = n + e
@@ -32,7 +31,6 @@ export function payloadGen602(t, site, appid, uuid, seq, cs, sid, vid, pxhd): St
     vid != undefined  && c.push("vid=" + vid); //vid
     pxhd  != undefined && c.push("pxhd=" + pxhd); //pxhd cookie
     c.push('rsc=' + (seq <= 1 ? seq+1 : seq));
-
     return c
 }
 
